88492a5d251ea7d3708c84614848425a3f1689f5
Aaron <aaronrod@gmail.com>
2017-06-07 11:26:51 +0100

    Added function to increase temperature with a function 'up'

--- jasmine-standalone-2.6.2/spec/FeatureSpec.js
+++ jasmine-standalone-2.6.2/spec/FeatureSpec.js
@@ -8,4 +8,9 @@ describe('Thermostat', function() {
   it ("should start at 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it ("should have increase the temperature with an Up function", function(){
+  	thermostat.up(1)
+  	expect(thermostat.temperature).toEqual(21);
+  });
 });
--- jasmine-standalone-2.6.2/src/Thermostat.js
+++ jasmine-standalone-2.6.2/src/Thermostat.js
@@ -1,3 +1,6 @@
 function Thermostat(temperature = 20) {
   this.temperature = temperature
+  this.up = function(temperature){
+  	this.temperature += 1;
+  };
 };
