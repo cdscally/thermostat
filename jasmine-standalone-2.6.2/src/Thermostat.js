60d6695ce149616adb396d6eaa1538f7584916eb
Aaron <aaronrod@gmail.com>
2017-06-07 14:16:06 +0100

    Refactored constants MEDIUM_ENERGY_USAGE_LIMIT and LOW_ENERGY_USAGE_LIMIT

--- jasmine-standalone-2.6.2/src/Thermostat.js
+++ jasmine-standalone-2.6.2/src/Thermostat.js
@@ -4,6 +4,8 @@ function Thermostat(temperature = 20, powersave = true) {
   this.powersave = powersave;
   this.maximum = 25;
   this.DEFAULT_TEMPERATURE = 20;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 25;
+  this.LOW_ENERGY_USAGE_LIMIT = 18;
 
   this.up = function(increment){
   	this.temperature += increment;
@@ -34,9 +36,9 @@ function Thermostat(temperature = 20, powersave = true) {
   };
 
   this.energyUsage = function() {
-  	if (this.temperature > 25) {
+  	if (this.temperature > this.MEDIUM_ENERGY_USAGE_LIMIT) {
   		return 'high-usage'
-  	}else if (this.temperature > 18 ) {
+  	}else if (this.temperature > this.LOW_ENERGY_USAGE_LIMIT ) {
   		return 'medium-usage'
   	}else{
   		return 'low-usage'
